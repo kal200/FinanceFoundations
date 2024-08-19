a8672f61cfe3a1f5c9eb15f4713606988e5a1be1
kal200 <kalippert35@gmail.com>
2024-08-19 07:36:48 -0400

    updated prefixes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,17 +6,17 @@ class Navbar extends React.Component {
         <header>
           <div className="container">
             <div className="logo">
-              <a href="/FinanceFoundations/index.html" target="_self">
-                <img src="/FinanceFoundations/assets/images/gradientfinancelogonobgresized.png" alt="logo" />
+              <a href="https://kal200.github.io/FinanceFoundations/index.html" target="_self">
+                <img src="https://kal200.github.io/FinanceFoundations/assets/images/gradientfinancelogonobgresized.png" alt="logo" />
               </a>
             </div>
             <div className="links">
               <ul>
-                <li><a href="/FinanceFoundations/pages/definitions.html">Definitions</a></li>
-                <li><a href="/FinanceFoundations/pages/calculators.html">Calculators</a></li>
-                <li><a href="/FinanceFoundations/pages/quizzes.html">Quizzes</a></li>
-                <li><a href="/FinanceFoundations/pages/adtlresources.html">Additional Resources</a></li>
-                <li><a href="/FinanceFoundations/pages/references.html">Attributions</a></li>
+                <li><a href="https://kal200.github.io/FinanceFoundations/pages/definitions.html">Definitions</a></li>
+                <li><a href="https://kal200.github.io/FinanceFoundations/pages/calculators.html">Calculators</a></li>
+                <li><a href="https://kal200.github.io/FinanceFoundations/pages/quizzes.html">Quizzes</a></li>
+                <li><a href="https://kal200.github.io/FinanceFoundations/pages/adtlresources.html">Additional Resources</a></li>
+                <li><a href="https://kal200.github.io/FinanceFoundations/pages/references.html">Attributions</a></li>
               </ul>
             </div>
           </div>
